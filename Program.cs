c21017eda77009045cc275e1e1dac06963145675
thutasann <thutasann2002@gmail.com>
2024-05-31 23:59:04 +0800

    feat: DiscordClient

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_discord_bot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace CSharp_Discord_Bot
+{
+    public class Program
+    {
+        private static DiscordClient? Client { get; set; }
+        private static CommandsNextExtension? Commands { get; set; }
+
+        public static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSONAsync();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+            await Client.ConnectAsync();
+            await Task.Delay(-1); // to keep bot running forever, as long as the program is running
+        }
+    }
+}
---
+++ config/JSONReader.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json;
+
+namespace csharp_discord_bot.config
+{
+    public class JSONReader
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Prefix { get; set; } = string.Empty;
+
+        public async Task ReadJSONAsync()
+        {
+            try
+            {
+                using StreamReader str = new("config.json");
+                string json = await str.ReadToEndAsync();
+                JSONStructure? data = JsonConvert.DeserializeObject<JSONStructure>(json);
+                if (data is not null)
+                {
+                    Token = data.Token;
+                    Prefix = data.Prefix;
+                }
+                else
+                {
+                    Console.WriteLine("No Data found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in reading json file : {ex.Message}");
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Prefix { get; set; } = string.Empty;
+    }
+}
