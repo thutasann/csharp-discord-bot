50a1a12db0ba0ad7764d3b26ec757b0c8d6239da
thutasann <thutasann2002@gmail.com>
2024-06-02 17:39:38 +0800

    feat: slash commands group

--- Commands/Slash/CalculatorSL.cs
+++ Commands/Slash/CalculatorSL.cs
@@ -1,9 +1,40 @@
+using System.Runtime.InteropServices;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 
 namespace csharp_discord_bot.Commands.Slash
 {
+    [SlashCommandGroup("calculator", "Perform calculator operations")]
     public class CalculatorSL : ApplicationCommandModule
     {
+        [SlashCommand("add", "Add 2 numbers together")]
+        public async Task Add(InteractionContext ctx, [Option("number1", "Number 1")] double number1, [Option("number2", "Number 2")] double number2)
+        {
+            await ctx.DeferAsync();
 
+            var outputEmbed = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Green,
+                Title = $"{number1} + {number2}",
+                Description = $"{number1 + number2}"
+            };
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(outputEmbed));
+        }
+
+        [SlashCommand("subtract", "Subtract 2 numbers together")]
+        public async Task Subtract(InteractionContext ctx, [Option("number1", "Number 1")] double number1, [Option("number2", "Number 2")] double number2)
+        {
+            await ctx.DeferAsync();
+
+            var outputEmbed = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Green,
+                Title = $"{number1} - {number2}",
+                Description = $"{number1 - number2}"
+            };
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().AddEmbed(outputEmbed));
+        }
     }
 }
